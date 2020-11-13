35abcf1c88cc0181495ed897628e4d04e01e25f7
mccor2000 <roccm0200@gmail.com>
2020-11-14 03:51:53 +0700

    feat: init roadmap module routes

--- src/modules/roadmap/route/index.js
+++ src/modules/roadmap/route/index.js
@@ -1,9 +1,32 @@
 import { Router } from 'express'
 
-import { authenticate } from '../../../middlewares'
+import controller from '../controller'
+import { authenticate, authorize, validate } from '../../../middlewares'
 
 const router = Router()
 
 router.use(authenticate('jwt'))
+
+router
+  .route('/')
+  .get(authorize([]), () => {})
+  .post(authorize([]), validate(), () => {})
+
+router
+  .route('/:roadmapId')
+  .get(authorize([]), () => {})
+  .put(authorize([]), validate(), () => {})
+  .delete(authorize([]), () => {})
+
+router
+  .route('/:roadmapId/nodes')
+  .get(authorize([]), () => {})
+  .post(authorize([]), validate(), () => {})
+
+router
+  .route('/:roadmapId/nodes/:nodeId')
+  .get(authorize([]), () => {})
+  .put(authorize([]), validate(), () => {})
+  .delete(authorize([]), () => {})
 
 export default router
