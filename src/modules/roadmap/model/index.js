7f47454ef636b7a2714f134efa4c2e9e20871129
mccor2000 <roccm0200@gmail.com>
2020-11-14 00:20:08 +0700

    feat: add roadmap model

--- src/modules/roadmap/model/index.js
+++ src/modules/roadmap/model/index.js
@@ -1,5 +1,80 @@
 import { model, Schema } from 'mongoose'
 
-const Roadmap = new Schema({}, { timestamps: true })
+const Leave = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+
+  references: [
+    {
+      type: String,
+      default: [],
+    },
+  ],
+})
+
+const Node = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+
+  childrens: [
+    {
+      type: Node,
+      default: [],
+    },
+  ],
+
+  leaves: [
+    {
+      type: Leave,
+      default: [],
+    },
+  ],
+})
+
+const Roadmap = new Schema(
+  {
+    field: {
+      type: String,
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+
+    author: [
+      {
+        type: String,
+      },
+    ],
+
+    rating: [
+      {
+        userId: {
+          type: Schema.Types.ObjectId,
+          ref: 'users',
+        },
+
+        point: {
+          type: Number,
+          default: 0,
+        },
+      },
+    ],
+
+    content: [
+      {
+        type: Node,
+        default: [],
+      },
+    ],
+  },
+  { timestamps: true }
+)
 
 export default model('Roadmap', Roadmap, 'roadmaps')
