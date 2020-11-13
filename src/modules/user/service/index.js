6bc6831acd5bf1722728b0f6a4b00988f427f9ca
mccor2000 <roccm0200@gmail.com>
2020-11-13 23:13:13 +0700

    refactor: remove unused services due to model's update

--- src/modules/user/service/index.js
+++ src/modules/user/service/index.js
@@ -1,4 +1,3 @@
-import User from '../model'
 import { AppError, ErrorType } from '../../../utils/errors'
 
 const getProfile = async (user) => {
@@ -20,78 +19,15 @@ const changePassword = async (user, { password, newPassword }) => {
     )
 
   const isValidPassword = await user.comparePassword(password)
-
   if (!isValidPassword)
     throw new AppError(ErrorType.UNAUTHORIZED, `Password incorrect`)
 
-  user.credentials.password = newPassword
+  user.password = newPassword
   await user.save()
-}
-
-const getAllRegisteredCourses = async (user) => {
-  return user.courses
-}
-
-const registerCourse = async (user, courseId) => {
-  user.courses.push({ courseId })
-
-  await user.save()
-}
-
-const getRegistedCourse = async (user, courseId) => {
-  const idx = user.courses.findIndex(
-    (registeredCourse) => registeredCourse.courseId == courseId
-  )
-  if (idx < 0) throw new AppError(ErrorType.BAD_REQUEST, `Course not found`)
-  const registeredCourse = user.courses[idx]
-
-  return registeredCourse
-}
-
-const markAsRead = async (user, courseId, lessonId) => {
-  const idx = user.courses.findIndex(
-    (registeredCourse) => registeredCourse.courseId == courseId
-  )
-  user.courses[idx].finished.push(lessonId)
-
-  await user.save()
-}
-
-const getStaffs = async () => {
-  const staffs = await User.find({ role: 'staff' })
-    .select('email profile')
-    .exec()
-
-  return staffs
-}
-
-const addStaff = async (id) => {
-  const newStaff = await User.findByIdAndUpdate(id, { role: 'staff' })
-    .select('email profile')
-    .exec()
-
-  return newStaff
-}
-
-const removeStaff = async (id) => {
-  const removedStaff = await User.findByIdAndUpdate(id, { role: 'user' })
-    .select('email profile')
-    .exec()
-
-  return removedStaff
 }
 
 export default {
   getProfile,
   updateProfile,
   changePassword,
-
-  getAllRegisteredCourses,
-  registerCourse,
-  getRegistedCourse,
-  markAsRead,
-
-  getStaffs,
-  addStaff,
-  removeStaff,
 }
